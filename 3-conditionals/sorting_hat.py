6ae321f49aab7909c9a1b0ece07cbf6df3499572
Sonny Li <sonnynomnom@gmail.com>
2022-06-06 13:46:30 -0400

    Update sorting_hat.py

--- 3-conditionals/sorting_hat.py
+++ 3-conditionals/sorting_hat.py
@@ -10,18 +10,69 @@ print("===============")
 print("The Sorting Hat")
 print("===============")
 
+# ~~~~~~~~~~~~~~~ Question 1 ~~~~~~~~~~~~~~~
+
 print("Q1) Do you like Dawn or Dusk?")
 
 print("  1) Dawn")
 print("  2) Dusk")
 
-answer1 = input("Enter your answer (1-2): ")
+answer = int(input("Enter answer (1-2): "))
 
-if answer1 == "1":
+if answer == 1:
   gryffindor += 1
   ravenclaw += 1
-elif answer1 == "2":
+elif answer == 2:
   hufflepuff += 1
   slytherin +=1
 else:
-  print("Invalid input.")
+  print("Wrong input.")
+
+# ~~~~~~~~~~~~~~~ Question 2 ~~~~~~~~~~~~~~~
+
+print("\nQ2) When I'm dead, I want people to remember me as:")
+
+print("  1) The Good")
+print("  2) The Great")
+print("  3) The Wise")
+print("  4) The Bold")
+
+answer = int(input("Enter your answer (1-4): "))
+
+if answer == 1:
+  hufflepuff += 1
+elif answer == 2:
+  slytherin +=1
+elif answer == 3:
+  ravenclaw +=1
+elif answer == 4:
+  gryffindor +=1
+else:
+  print("Wrong input.")
+
+# ~~~~~~~~~~~~~~~ Question 3 ~~~~~~~~~~~~~~~
+
+print("\nQ3) Which kind of instrument most pleases your ear?")
+
+print("  1) The violin")
+print("  2) The trumpet")
+print("  3) The piano")
+print("  4) The drum")
+
+answer = int(input("Enter your answer (1-4): "))
+
+if answer == 1:
+  slytherin +=1
+elif answer == 2:
+  hufflepuff +=1
+elif answer == 3:
+  ravenclaw +=1
+elif answer == 4:
+  gryffindor +=1
+else:
+  print("Wrong input.")
+  
+print(gryffindor)
+print(ravenclaw)
+print(hufflepuff)
+print(slytherin)
