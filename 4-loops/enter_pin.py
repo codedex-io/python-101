fa9aab6b8fa848d1bb90e8cec43ed77c16e32939
Chuck <asdeoz@users.noreply.github.com>
2022-08-05 01:53:27 -0700

    Transform input to int
    Without transforming the input to an int it will never get out of the loop since it will be trying to compare a string to a number.

--- 4-loops/enter_pin.py
+++ 4-loops/enter_pin.py
@@ -6,7 +6,7 @@ print("=== BANK OF CODéDEX ===")
 pin = int(input("Enter your PIN: "))
 
 while pin != 1234:
-  pin = input("Incorrect PIN. Enter your PIN again: ")
+  pin = int(input("Incorrect PIN. Enter your PIN again: "))
 
 if pin == 1234:
   print("PIN accepted!")
