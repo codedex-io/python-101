a82507edeb282d96e31c12a2cfc95435b9e52edc
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-09-30 11:02:27 -0400

    Add Solutions
    Added solutions for the lists chapter

---
+++ 5-lists/inventory.py
@@ -0,0 +1,6 @@
+# Inventory ✈️
+# Codédex
+
+airplane_toys = [ 898, 732, 543, 878 ]
+print(min(airplane_toys))
+print(max(airplane_toys))
---
+++ 5-lists/reading.py
@@ -0,0 +1,15 @@
+# Reading 📚
+# Codédex
+
+books = ['Zero to One',
+        'The Lean Startup',
+        'The Mom Test',
+        'Made to Stick',
+        'Life in Code']
+
+print(books)
+
+books.remove('Zero to One')
+books.pop(0)
+
+print(books)
