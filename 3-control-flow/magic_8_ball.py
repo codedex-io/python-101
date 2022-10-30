5d22edab9f5e37387efb7c43537f92e30f554708
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-10-30 18:36:25 -0400

    Update magic_8_ball.py

--- 3-control-flow/magic_8_ball.py
+++ 3-control-flow/magic_8_ball.py
@@ -3,31 +3,31 @@
 
 import random
 
-question = input("Question: ")
+question = input('Question: ')
 
 random_number = random.randint(1, 9)
 # print(random_number)
 
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = 'Yes - definitely'
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = 'It is decidedly so'
 elif random_number == 3:
-  answer = "Without a doubt"
+  answer = 'Without a doubt'
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = 'Reply hazy, try again'
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = 'Ask again later'
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = 'Better not tell you now'
 elif random_number == 7:
-  answer = "My sources say no"
+  answer = 'My sources say no'
 elif random_number == 8:
-  answer = "Outlook not so good"
+  answer = 'Outlook not so good'
 elif random_number == 9:
-  answer = "Very doubtful"
+  answer = 'Very doubtful'
 else:
-  answer = "Error"
+  answer = 'Error'
   
 print("Question:      " + question)
 print("Magic 8 Ball:  " + answer)
