b726ed859e9e2fa342b7b0d6dcd5f974c2391077
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-10-30 18:34:22 -0400

    Update currency.py

--- 2-variables/currency.py
+++ 2-variables/currency.py
@@ -1,9 +1,9 @@
 # Currency 💵
 # Codédex
 
-yuan = int(input("What do you have left in yuan? "))
-yen = int(input("What do you have left in yen? "))
-won = int(input("What do you have left in won? "))
+yuan = int(input('What do you have left in yuan? '))
+yen = int(input('What do you have left in yen? '))
+won = int(input('What do you have left in won? '))
 
 total = yuan * 0.15 + yen * 0.0077 + won * 0.00080
 
