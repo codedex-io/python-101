d441a0ed85bc74fc0ed8b6e3d1f62aad17fa121a
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-10-30 19:10:44 -0400

    Update fizz_buzz.py

--- 4-loops/fizz_buzz.py
+++ 4-loops/fizz_buzz.py
@@ -3,10 +3,10 @@
 
 for num in range(1, 101):
   if num % 3 == 0 and num % 5 == 0:
-    print("FizzBuzz")
+    print('FizzBuzz')
   elif num % 3 == 0:
-    print("Fizz")
+    print('Fizz')
   elif num % 5 == 0:
-    print("Buzz")
+    print('Buzz')
   else:
     print(num)
