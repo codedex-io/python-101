323b3a81a03d84177222080e970023dd958a46be
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-10-30 19:08:30 -0400

    Update 99_bottles.py

--- 4-loops/99_bottles.py
+++ 4-loops/99_bottles.py
@@ -2,7 +2,7 @@
 # Codédex
 
 for i in range(99, 0, -1):
-  print(f"{i} bottles of beer on the wall")
-  print(f"{i} bottles of beer")
-  print("take one down pass it around")
-  print(f"{i-1} bottles of beer on the wall")
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('take one down pass it around')
+  print(f'{i-1} bottles of beer on the wall')
