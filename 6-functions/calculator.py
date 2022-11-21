4b3ed5dae1080ee5f85531f60b364515e69703af
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-11-21 12:34:40 -0500

    Add functions solutions

---
+++ 6-functions/calculator.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def exp(a, b):
+    return a ** b
+
+print(add(3, 5))
+print(subtract(7, 2))
+print(multiply(4, 8))
+print(divide(9, 3))
+print(exp(2, 3))
