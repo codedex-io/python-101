1b69cc7febcce246a94341203f41a0385a567335
Sonny Li <sonnynomnom@gmail.com>
2022-11-21 18:59:49 -0500

    Update rocket.py

--- 6-functions/rocket.py
+++ 6-functions/rocket.py
@@ -1,5 +1,8 @@
-# Mars Orbiter
+# Mars Orbiter 🚀
 # Codédex
 
 def distance_to_miles(distance):
-    return distance / 1.609
+  return distance / 1.609
+
+answer = distance_to_miles(10000)
+print(answer)
