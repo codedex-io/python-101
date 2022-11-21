94fb6fd70d5ba6ba2c7602bb34eda0594977395a
Sonny Li <sonnynomnom@gmail.com>
2022-11-21 18:50:41 -0500

    Update dry.py

--- 6-functions/dry.py
+++ 6-functions/dry.py
@@ -1,14 +1,21 @@
+# D.R.Y. 🧩
+# Codédex
+
 import random
 
 list_of_foods = ['celery', 'broccoli', 'cabbage']
 
-# This was the first function introduced in the course, and prints the statement in the closed brackets
-print("Hello, World!")
+# This was the first function introduced in the course
+print('Hello, World!')
+
 # This function calculates the maximum of two numbers a and b
 print(max(3, 5))
+
 # This function calculates the minmum of two numbers a and b
 print(min(-1, 7))
+
 # This function calculates the length of a list
 print(len(list_of_foods))
-# This function finds a random number between two values inclusive
-print(random.randint(2, 8))
+
+# This function calculates the a to the power b
+print(pow(2, 6))
