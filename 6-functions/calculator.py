a1da02e8c8ae000585053da931945ac8bf586c77
Sonny Li <sonnynomnom@gmail.com>
2022-11-21 19:01:04 -0500

    Update calculator.py

--- 6-functions/calculator.py
+++ 6-functions/calculator.py
@@ -1,20 +1,20 @@
-# Calculator
+# Calculator 🔢
 # Codédex
 
 def add(a, b):
-    return a + b
+  return a + b
 
 def subtract(a, b):
-    return a - b
+  return a - b
 
 def multiply(a, b):
-    return a * b
+  return a * b
 
 def divide(a, b):
-    return a / b
+  return a / b
 
 def exp(a, b):
-    return a ** b
+  return a ** b
 
 print(add(3, 5))
 print(subtract(7, 2))
