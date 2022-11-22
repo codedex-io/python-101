c10e7f5a2d98e96468d5636bb3377ac3ef737cfd
Sonny Li <sonnynomnom@gmail.com>
2022-11-21 19:05:15 -0500

    Update drive_thru.py

--- 6-functions/drive_thru.py
+++ 6-functions/drive_thru.py
@@ -1,23 +1,30 @@
-# Drive Thru
+# Drive-Thru 🚙
 # Codédex
 
 def get_item(x):
-    if x == 1:
-        return '🍔 Cheeseburger'
-    elif x == 2:
-        return '🍟 Fries'
-    elif x == 3:
-        return '🥤 Soda'
-    elif x == 4:
-        return '🍦 Ice Cream'
-    elif x == 5:
-        return '🍪 Cookie'
-    else:
-        return "invalid option"
+  if x == 1:
+    return '🍔 Cheeseburger'
+  elif x == 2:
+    return '🍟 Fries'
+  elif x == 3:
+    return '🥤 Soda'
+  elif x == 4:
+    return '🍦 Ice Cream'
+  elif x == 5:
+    return '🍪 Cookie'
+  else:
+    return "invalid option"
 
 def welcome():
-    print('Welcome to The Sonny Restaurant! What would you like to order')
+  print('Welcome to Sonnyboy\'s Diner!')
+  print('Here\'s the menu:')
+  print('1. 🍔 Cheeseburger')
+  print('2. 🍟 Fries')
+  print('3. 🥤 Soda')
+  print('4. 🍦 Ice Cream')
+  print('5. 🍪 Cookie')
 
 welcome()
+
 option = int(input('What would you like to order? '))
 print(get_item(option))
