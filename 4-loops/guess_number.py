2ce5e8c0e750ca3418960ec517fd4a21f421c42b
JavitoCoder <120044691+JavitoCoder@users.noreply.github.com>
2022-12-14 08:31:20 -0400

    Update guess_number.py
    it's better and easier this way

--- 4-loops/guess_number.py
+++ 4-loops/guess_number.py
@@ -4,11 +4,12 @@
 guess = 0
 tries = 0
 
-while guess != 6 and tries < 5:
-  guess = int(input('Guess the number:  '))
-  tries = tries + 1
+while guess != 6 and tries < 5 :
+    guess = int(input("Guess the number:  "))
+    tries += 1
+    if guess == 6:
+        print('You Got it!')
+        
+if tries == 5:
+    print("You ran out of tries :(")
 
-if guess != 6:
-  print('You ran out of tries.')
-else:
-  print('You got it!')
