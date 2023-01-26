ab6f88b8a250f882ee611a0f89098a6e09e1c6a0
Brandon Dusch <brandondusch@gmail.com>
2023-01-26 16:30:38 -0500

    Update fortune_cookie.py

--- 6-functions/fortune_cookie.py
+++ 6-functions/fortune_cookie.py
@@ -3,7 +3,8 @@
 
 import random
 
-options = ['Don’t pursue happiness – create it.',
+options = [
+  'Don’t pursue happiness – create it.',
   'All things are difficult before they are easy.',
   'The early bird gets the worm, but the second mouse gets the cheese.',
   'If you eat something and nobody sees you eat it, it has no calories.',
@@ -11,10 +12,34 @@ options = ['Don’t pursue happiness – create it.',
   'Don’t just think. Act!',
   'Your heart will skip a beat.',
   'The fortune you search for is in another cookie.',
-  'Help! I’m being held prisoner in a Chinese bakery!']
+  'Help! I’m being held prisoner in a Chinese bakery!'
+]
 
 def fortune():
-  print(options[random.randint(0, len(options) - 1)])
+  random_fortune = random.randint(0, len(options) - 1)
+
+  if random_fortune == 0:
+    option = options[0]
+  elif random_fortune == 1:
+    option = options[1]
+  elif random_fortune == 2:
+    option = options[2]
+  elif random_fortune == 3:
+    option = options[3]
+  elif random_fortune == 4:
+    option = options[4]
+  elif random_fortune == 5:
+    option = options[5]
+  elif random_fortune == 6:
+    option = options[6]
+  elif random_fortune == 7:
+    option = options[7]
+  elif random_fortune == 8:
+    option = options[7]
+  else:
+    option = 'Error'
+
+  print(option)
 
 fortune()
 fortune()
