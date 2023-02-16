341c3cd4833d9e399e3622df83c427f246b903a6
Brandon Dusch <brandondusch@gmail.com>
2023-02-16 12:58:54 -0500

    small edits; remove breakpoints

--- 3-control-flow/15_the_cyclone.py
+++ 3-control-flow/15_the_cyclone.py
@@ -2,20 +2,17 @@
 # Codédex
 
 myHeight = 250
-credits = 9
+credits = 10
 withTallerPerson = False
 
 if myHeight >= 200 and credits >= 10:
-  breakpoint()
   print("Enjoy the ride!")
 
 if myHeight < 200:
   if myHeight < 100 or not withTallerPerson:
-    breakpoint()
     print("You're not tall enough for this ride yet.")
   elif myHeight >= 100 and withTallerPerson:
-     breakpoint()
-     print("Enjoy the ride!")
+    print("Enjoy the ride!")
 
 if credits < 10:
   print("You don't have enough credits to ride.")
