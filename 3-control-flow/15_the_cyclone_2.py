b3ea86eb2c8228de9965151480d807c5fe2b66b5
Dusch4593 <brandondusch@gmail.com>
2023-03-11 12:58:50 -0500

    revert solution #2 for The Cyclone

--- 3-control-flow/15_the_cyclone_2.py
+++ 3-control-flow/15_the_cyclone_2.py
@@ -5,15 +5,14 @@ height = 250
 credits = 10
 with_taller_person = False
 
+if height >= 137 and credits >= 10:
+  print("Enjoy the ride!")
+
+if height < 137:
+  if height < 100 or not with_taller_person:
+    print("You're not tall enough for this ride yet.")
+  elif height >= 100 and with_taller_person:
+    print("Enjoy the ride!")
+
 if credits < 10:
   print("You don't have enough credits to ride.")
-else:
-  if height >= 137 and credits >= 10:
-    print("Enjoy the ride!")
-  elif height < 137:
-    if height < 100 or not with_taller_person:
-      print("You're not tall enough for this ride yet.")
-    elif height >= 100 and with_taller_person:
-      print("Enjoy the ride, folks!")
-  else:
-    print("Invalid data.")
