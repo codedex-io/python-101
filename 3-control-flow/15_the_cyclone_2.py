b289e8b03f773a89e8ddb21aa29420bd7813484a
vance <caesiumtea@gmail.com>
2023-03-12 00:51:53 -0500

    Update 3-control-flow/15_the_cyclone_2.py
    Move check for `credits` to the start of the control flow
    
    Co-authored-by: Brandon Dusch <brandondusch@gmail.com>

--- 3-control-flow/15_the_cyclone_2.py
+++ 3-control-flow/15_the_cyclone_2.py
@@ -5,10 +5,10 @@ height = 250
 credits = 10
 with_taller_person = False
 
-if height >= 137 and credits >= 10:
-  print("Enjoy the ride!")
-elif credits < 10:
+if credits < 10:
   print("You don't have enough credits to ride.")
+elif height >= 137 and credits >= 10:
+  print("Enjoy the ride!")
 elif height < 137:
   if height < 100 or not with_taller_person:
     print("You're not tall enough for this ride yet.")
