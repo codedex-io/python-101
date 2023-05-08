e87d21c5e767efdc364ed3ead3dbcfda473bc31e
Brandon Dusch <brandondusch@gmail.com>
2023-05-08 00:09:45 -0400

    Update 41_main.py

--- 8-modules/41_main.py
+++ 8-modules/41_main.py
@@ -13,4 +13,4 @@ days_away = my_next_birthday - today
 if my_next_birthday == today:
   print(bday_messages.random_message)
 else:
-  print(f'My next birthday is {days_away} away!')
+  print(f'My next birthday is {days_away.days} days away!')
