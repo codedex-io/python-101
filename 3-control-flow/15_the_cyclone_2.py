ba922d0e21ddac5dcc841a2e940d3390a6e53502
Brandon Dusch <brandon@codedex.io>
2023-07-26 09:16:54 -0400

    Update 15_the_cyclone_2.py

--- 3-control-flow/15_the_cyclone_2.py
+++ 3-control-flow/15_the_cyclone_2.py
@@ -6,12 +6,12 @@ is_open = True
 height = int(input('What is your height (cm)? '))
 credits = int(input('How many credits do you have? '))
 
-not_tall_enough = height < 137 and credits >= 10
-not_enough_credits = credits < 10 and height >= 137
+tall_enough = height >= 137
+not_enough_credits = credits < 10
 
 if not is_open:
   print("Sorry! The ride is currently closed!")
-elif not_tall_enough or not_enough_credits:
+elif not tall_enough or not_enough_credits:
   print("You are either not tall enough to ride or you don't have enough credits.")
 else:
   print("Enjoy the ride!")
