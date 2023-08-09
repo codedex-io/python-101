9e5d0182d333002f9dfe62835242dfbece2ef2f0
Dusch4593 <brandondusch@gmail.com>
2023-08-09 10:23:04 -0400

    combine both files for exercise 41 into one file

--- 8-modules/41_bday_messages.py
+++ 8-modules/41_bday_messages.py
@@ -1,7 +1,7 @@
 # Countdown 🎂
 # Codédex
 
-import random
+import random, datetime
 
 bday_messages = [
   'Hope you have a very Happy Birthday! 🎈',
@@ -12,3 +12,14 @@ bday_messages = [
 ]
 
 random_message = random.choice(bday_messages)
+
+today = datetime.date.today()
+
+my_next_birthday = datetime.date(2023, 4, 5)
+
+days_away = my_next_birthday - today
+
+if my_next_birthday == today:
+  print(bday_messages.random_message)
+else:
+  print(f'My next birthday is {days_away.days} days away!')
