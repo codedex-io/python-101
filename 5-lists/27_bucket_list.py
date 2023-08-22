1a1a23fd52eb0986d179f68f2aa635119fd26cd9
Sonny Li <sonnynomnom@gmail.com>
2023-08-22 02:11:33 -0400

    Update 27_bucket_list.py

--- 5-lists/27_bucket_list.py
+++ 5-lists/27_bucket_list.py
@@ -3,11 +3,11 @@
 
 things_to_do = [
    '🏃 Finish a 10k marathon.',
+   '📝 Publish a short story.',
    '🌏 Visit at least 10 countries in my lifetime.',
    '🚲 Try out mountain biking.',
-   '🚀 Build a menaingful product for everyone.',
    '🎸 Produce an original song.',
-   '📝 Write a short story.'
+   '🚀 Build a menaingful product for everyone.',
 ]
 
 for thing in things_to_do:
