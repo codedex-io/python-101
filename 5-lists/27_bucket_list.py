cefbbb5b909be511fc725d32ad86f68c2db55055
Sonny Li <sonnynomnom@gmail.com>
2023-08-22 02:10:57 -0400

    Update 27_bucket_list.py

--- 5-lists/27_bucket_list.py
+++ 5-lists/27_bucket_list.py
@@ -2,12 +2,12 @@
 # Codédex
 
 things_to_do = [
+   '🏃 Finish a 10k marathon.',
    '🌏 Visit at least 10 countries in my lifetime.',
    '🚲 Try out mountain biking.',
    '🚀 Build a menaingful product for everyone.',
    '🎸 Produce an original song.',
-   '📝 Write a short story.',
-   '🏃 Finish a 10k marathon.'
+   '📝 Write a short story.'
 ]
 
 for thing in things_to_do:
