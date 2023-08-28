a4a9c5f6043a45b1b9003af05d8b9dbd9dc45165
Sonny Li <sonnynomnom@gmail.com>
2023-08-28 02:29:05 -0400

    Update 28_dry.py

--- 6-functions/28_dry.py
+++ 6-functions/28_dry.py
@@ -3,10 +3,10 @@
 
 import random
 
-list_of_foods = ['celery', 'broccoli', 'cabbage']
+coffee_menu = ['drip coffee', 'cold brew', 'cappuccino', 'latte', 'mocha', 'espresso']
 
 # This was the first function introduced in the course
-print('Hello, World!')
+print('Hello World!')
 
 # This function calculates the maximum of two numbers a and b
 print(max(3, 5))
@@ -15,7 +15,7 @@ print(max(3, 5))
 print(min(-1, 7))
 
 # This function calculates the length of a list
-print(len(list_of_foods))
+print(len(coffee_menu))
 
 # This function calculates a to the power b
 print(pow(2, 6))
