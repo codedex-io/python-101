3d0428e6a995feb6d0e9532a107a27e1deba8d18
Sonny Li <sonnynomnom@gmail.com>
2023-08-28 15:58:10 -0400

    Update 10_currency.py

--- 2-variables/10_currency.py
+++ 2-variables/10_currency.py
@@ -5,6 +5,6 @@ pesos = int(input('What do you have left in pesos? '))
 soles = int(input('What do you have left in soles? '))
 reais = int(input('What do you have left in reais? '))
 
-total = pesos * 0.00025 + soles * 0.28 + reais * 0.21
+total = pesos * 0.00024 + soles * 0.28 + reais * 0.21
 
 print(total)
