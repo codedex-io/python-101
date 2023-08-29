fc24d9058db71f87e33c697b54c82c06c308e34b
Sonny Li <sonnynomnom@gmail.com>
2023-08-29 00:42:26 -0400

    Update 29_fortune_cookie_1.py

--- 6-functions/29_fortune_cookie_1.py
+++ 6-functions/29_fortune_cookie_1.py
@@ -7,7 +7,6 @@ options = [
   'Don’t pursue happiness – create it.',
   'All things are difficult before they are easy.',
   'The early bird gets the worm, but the second mouse gets the cheese.',
-  'If you eat something and nobody sees you eat it, it has no calories.',
   'Someone in your life needs a letter from you.',
   'Don’t just think. Act!',
   'Your heart will skip a beat.',
