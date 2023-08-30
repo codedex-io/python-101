1bc03da2f15e0dc8d6c5431dc109dc1ee2a37f5c
Sonny Li <sonnynomnom@gmail.com>
2023-08-30 14:52:13 -0400

    Update 32_stonks.py

--- 6-functions/32_stonks.py
+++ 6-functions/32_stonks.py
@@ -3,9 +3,6 @@
 
 stock_prices = [34.68, 36.09, 34.94, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
 
-def price_at(i):
-  return stock_prices[i-1]
-
 def max_price(a, b):
   mx = 0
   for i in range(a, b + 1):
@@ -17,6 +14,9 @@ def min_price(a, b):
   for i in range(a, b + 1):
     mn = min(mn, price_at(i))
   return mn
+
+def price_at(i):
+  return stock_prices[i-1]
 
 print(max_price(1, 15))
 print(min_price(5, 10))
