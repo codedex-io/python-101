65d77e4f82835e3867b3dab4e1550f6e8ff12cbb
Sonny Li <sonnynomnom@gmail.com>
2023-09-01 21:33:50 -0400

    Update 35_bobs_burgers.py

--- 7-classes-objects/35_bobs_burgers.py
+++ 7-classes-objects/35_bobs_burgers.py
@@ -3,24 +3,24 @@
 
 class Restaurant:
   name = ''
-  type = ''
+  category = ''
   rating = 0.0
   delivery = False
 
 bobs_burgers = Restaurant()
 bobs_burgers.name = 'Bob\'s Burgers'
-bobs_burgers.type = 'American Diner'
+bobs_burgers.category = 'American Diner'
 bobs_burgers.rating = 4.2
 bobs_burgers.delivery = False
 
 katz_deli = Restaurant()
 katz_deli.name = 'Katz\'s Deli'
-katz_deli.type = 'Kosher Deli'
+katz_deli.category = 'Kosher Deli'
 katz_deli.rating = 4.5
 katz_deli.delivery = True
 
 baekjeong = Restaurant()
 baekjeong.name = 'Baekjeong NYC'
-baekjeong.type = 'Korean BBQ'
+baekjeong.category = 'Korean BBQ'
 baekjeong.rating = 4.4
 baekjeong.delivery = False
