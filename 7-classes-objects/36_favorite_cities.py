16de5826065bee3f3a11abe1b0929f7adf54d675
Sonny Li <sonnynomnom@gmail.com>
2023-09-01 21:41:03 -0400

    Update 36_favorite_cities.py

--- 7-classes-objects/36_favorite_cities.py
+++ 7-classes-objects/36_favorite_cities.py
@@ -8,7 +8,6 @@ class City:
     self.population = population
     self.landmarks = landmarks
 
-
 nyc = City("New York City", "USA", 8468000, ["Statue of Liberty", "Brooklyn Bridge", "Apollo Theatre"])
 
 shanghai = City("Shanghai", "China", 26320000, ["The Bund", "Jin Mao Tower", "Tianzifang"])
