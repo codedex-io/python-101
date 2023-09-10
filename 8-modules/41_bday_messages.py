ce49846608bd4a5ae944f7dd524f021153263be3
Sonny Li <sonnynomnom@gmail.com>
2023-09-10 13:05:23 -0400

    Update 41_bday_messages.py

--- 8-modules/41_bday_messages.py
+++ 8-modules/41_bday_messages.py
@@ -6,7 +6,7 @@ import random
 bday_messages = [
   'Hope you have a very Happy Birthday! 🎈',
   'It\'s your special day – get out there and celebrate! 🎉',
-  'You were born and the world got better – everybody wins! Happy Birthday! 🥳',
+  'You were born and the world got better – everybody wins! 🥳',
   'Have lots of fun on your special day! 🎂',
   'Another year of you going around the sun! 🌞'
 ]
