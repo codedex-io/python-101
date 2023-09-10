598d22398bb49cb93b92e9488739493fafb48985
Sonny Li <sonnynomnom@gmail.com>
2023-09-09 20:16:22 -0400

    Update 39_slot_machine_1.py

--- 8-modules/39_slot_machine_1.py
+++ 8-modules/39_slot_machine_1.py
@@ -14,6 +14,6 @@ results = random.choices(symbols, k=3)
 print(f'{results[0]} | {results[1]} | {results[2]}')
 
 if (results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣'):
- print('Jackpot! 💰')
+  print('Jackpot! 💰')
 else:
- print('Thanks for playing!')
+  print('Thanks for playing!')
