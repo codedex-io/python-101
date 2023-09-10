8175f4e599910a4d0ec74c3ddbbefc79ebfa0ff6
Sonny Li <sonnynomnom@gmail.com>
2023-09-10 17:41:42 -0400

    Update 41_main.py

--- 8-modules/41_main.py
+++ 8-modules/41_main.py
@@ -5,11 +5,11 @@ import datetime, bday_messages
 
 today = datetime.date.today()
 
-my_next_birthday = datetime.date(2023, 4, 5)
+next_birthday = datetime.date(2023, 4, 5)
 
-days_away = my_next_birthday - today
+days_away = next_birthday - today
 
-if my_next_birthday == today:
+if next_birthday == today:
   print(bday_messages.random_message)
 else:
   print(f'My next birthday is {days_away.days} days away!')
