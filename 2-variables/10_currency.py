0c23030c95649809aadbe6efc2e24d202f025da0
uio23 <98493983+uio23@users.noreply.github.com>
2023-10-14 12:49:13 +1300

    10_currency.py issue
    #hacktoberfest
    
    Round usd total to the nearest cent
    Inform the user that the output value is in USD

--- 2-variables/10_currency.py
+++ 2-variables/10_currency.py
@@ -6,5 +6,7 @@ soles = int(input('What do you have left in soles? '))
 reais = int(input('What do you have left in reais? '))
 
 total = pesos * 0.00025 + soles * 0.28 + reais * 0.21
+# Round to the nearest cent
+total = (int(100 * total - 0.5) + 1) / 100.0
 
-print(total)
+print('You have $' + str(total) + ' in USD.')
