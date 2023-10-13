fb2885be528ab2a9e3116edf88b8cb68afd58e19
uio23 <98493983+uio23@users.noreply.github.com>
2023-10-14 12:36:56 +1300

    16_sorting_hat_1.py issue
    #hacktoberfest
    
    Avoid casting user input to int in case the user enters a non-integer value, which would throw a runtime error.
    
    Instead, evaluate user input as a string.

--- 3-control-flow/16_sorting_hat_1.py
+++ 3-control-flow/16_sorting_hat_1.py
@@ -17,12 +17,12 @@ print('Q1) Do you like Dawn or Dusk?')
 print('  1) Dawn')
 print('  2) Dusk')
 
-answer = int(input('Enter answer (1-2): '))
+answer = input('Enter answer (1-2): ')
 
-if answer == 1:
+if answer == '1':
   gryffindor += 1
   ravenclaw += 1
-elif answer == 2:
+elif answer == '2':
   hufflepuff += 1
   slytherin +=1
 else:
@@ -37,15 +37,15 @@ print('  2) The Great')
 print('  3) The Wise')
 print('  4) The Bold')
 
-answer = int(input('Enter your answer (1-4): '))
+answer = input('Enter your answer (1-4): ')
 
-if answer == 1:
+if answer == '1':
   hufflepuff += 2
-elif answer == 2:
+elif answer == '2':
   slytherin += 2
-elif answer == 3:
+elif answer == '3':
   ravenclaw += 2
-elif answer == 4:
+elif answer == '4':
   gryffindor += 2
 else:
   print('Wrong input.')
@@ -59,15 +59,15 @@ print('  2) The trumpet')
 print('  3) The piano')
 print('  4) The drum')
 
-answer = int(input('Enter your answer (1-4): '))
+answer = input('Enter your answer (1-4): ')
 
-if answer == 1:
+if answer == '1':
   slytherin += 4
-elif answer == 2:
+elif answer == '2':
   hufflepuff += 4
-elif answer == 3:
+elif answer == '3':
   ravenclaw +=4
-elif answer == 4:
+elif answer == '4':
   gryffindor += 4
 else:
   print('Wrong input.')
