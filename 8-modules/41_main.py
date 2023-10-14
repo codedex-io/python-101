f2856640d7fdd00715afeb7fe856d73deb97157c
uio23 <98493983+uio23@users.noreply.github.com>
2023-10-14 14:06:39 +1300

    41_main.py suggestion
    #hacktoberfest
    
    Avoid computing days_away if next_birthday is == today.

--- 8-modules/41_main.py
+++ 8-modules/41_main.py
@@ -7,9 +7,9 @@ today = datetime.date.today()
 
 next_birthday = datetime.date(2023, 4, 5)
 
-days_away = next_birthday - today
 
 if next_birthday == today:
   print(bday_messages.random_message)
 else:
+  days_away = next_birthday - today
   print(f'My next birthday is {days_away.days} days away!')
