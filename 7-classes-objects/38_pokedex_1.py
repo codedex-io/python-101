79acb943c716ffffe9e1e3123b785dbcf88a4719
Dusch4593 <brandondusch@gmail.com>
2023-11-20 09:48:57 -0500

    add second solution for Pokemon exercise

---
+++ 7-classes-objects/38_pokedex_1.py
@@ -0,0 +1,39 @@
+# Pokédex 📟
+# Codédex
+
+# Class definition
+class Pokemon:
+  def __init__(self, entry, name, types, description, is_caught):
+    self.entry = entry
+    self.name = name
+    self.types = types
+    self.description = description
+    self.is_caught = is_caught
+
+  def speak(self):
+    print(self.name + ', ' + self.name + '!')
+  
+  def display_details(self):
+    print('Entry Number: ' + str(self.entry))
+    print('Name: ' + self.name)
+
+    if len(self.types) == 1:
+      print('Type: ' + self.types[0])
+    else:
+      print('Type: ' + self.types[0] + '/' + self.types[1])
+    
+    print('Description: ' + self.description)
+
+    if self.is_caught:
+      print(self.name + ' has already been caught!')
+    else:
+      print(self.name + ' hasn\'t been caught yet.')
+
+# Pokémon objects
+pikachu = Pokemon(25, 'Pikachu', ['Electric'], 'It has small electric sacs on both its cheeks. If threatened, it looses electric charges from the sacs.', True)
+charizard = Pokemon(3, 'Charizard', ['Fire', 'Flying'], 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.', False)
+gyarados = Pokemon(130, 'Gyarados', ['Water', 'Flying'], 'It has an extremely aggressive nature. The HYPER BEAM it shoots from its mouth totally incinerates all targets.', False)
+
+pikachu.speak()
+charizard.speak()
+gyarados.speak()
---
+++ 7-classes-objects/38_pokedex_2.py
@@ -0,0 +1,43 @@
+# Pokédex 📟
+# Codédex
+
+# Class definition
+class Pokemon:
+  def __init__(self, entry, name, types, description, level, region, is_caught):
+    self.entry = entry
+    self.name = name
+    self.types = types
+    self.description = description
+    self.level = level
+    self.region = region
+    self.is_caught = is_caught
+
+  def speak(self):
+    print(self.name + ', ' + self.name + '!')
+  
+  def display_details(self):
+    print('Entry Number: ' + str(self.entry))
+    print('Name: ' + self.name)
+
+    if len(self.types) == 1:
+      print('Type: ' + self.types[0])
+    else:
+      print('Type: ' + self.types[0] + '/' + self.types[1])
+    
+    print('Lv. ' + str(self.level))
+    print('Region: ' + self.region)
+    print('Description: ' + self.description)
+
+    if self.is_caught:
+      print(self.name + ' has already been caught!')
+    else:
+      print(self.name + ' hasn\'t been caught yet.')
+
+# Pokémon objects
+pikachu = Pokemon(25, 'Pikachu', ['Electric'], 'It has small electric sacs on both its cheeks. If threatened, it looses electric charges from the sacs.', 25, 'Kanto', True)
+charizard = Pokemon(3, 'Charizard', ['Fire', 'Flying'], 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.', 36, 'Kanto', False)
+gyarados = Pokemon(130, 'Gyarados', ['Water', 'Flying'], 'It has an extremely aggressive nature. The HYPER BEAM it shoots from its mouth totally incinerates all targets.', 57, 'Kanto', False)
+
+pikachu.speak()
+charizard.speak()
+gyarados.speak()
