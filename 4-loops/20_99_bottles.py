444e49ab4d44aaabe041e1e87f3eacbe2bc2f639
Sonny Li <sonnynomnom@gmail.com>
2024-01-24 09:07:42 -0500

    Update 20_99_bottles.py

--- 4-loops/20_99_bottles.py
+++ 4-loops/20_99_bottles.py
@@ -4,5 +4,5 @@
 for i in range(99, 0, -1):
   print(f'{i} bottles of beer on the wall')
   print(f'{i} bottles of beer')
-  print('take one down pass it around')
+  print('Take one down, pass it around')
   print(f'{i-1} bottles of beer on the wall')
