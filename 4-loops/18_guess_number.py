97d8bbb5255c5afbdab329f42a01fc7e76353248
Anjalisharma8072 <anjaliiisharma007@gmail.com>
2024-10-11 22:55:20 +0530

    added try-excpet block

--- 4-loops/18_guess_number.py
+++ 4-loops/18_guess_number.py
@@ -5,8 +5,12 @@ guess = 0
 tries = 0
 
 while guess != 6 and tries < 5:
-  guess = int(input('Guess the number:  '))
-  tries = tries + 1
+  try:
+    guess = int(input('Guess the number:  '))
+    tries = tries + 1
+  except ValueError:
+      print("Please enter a valid number.")
+
 
 if guess != 6:
   print('You ran out of tries.')
