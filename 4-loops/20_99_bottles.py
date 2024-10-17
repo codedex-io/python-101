60ffa49ad7170b9eef1e3745a76049d16596097b
Bruno Viola <99220989+BrunoViola@users.noreply.github.com>
2024-10-16 23:52:40 -0300

    Fix: pluralization logic for the word "Bottle"
    The code was printing 'bottles' for every case, then I made a correction to print 'bottle' when it has just one bottle.

--- 4-loops/20_99_bottles.py
+++ 4-loops/20_99_bottles.py
@@ -2,8 +2,12 @@
 # Codédex
 
 for i in range(99, 0, -1):
-  print(f'{i} bottles of beer on the wall')
-  print(f'{i} bottles of beer')
+  if(i>1):
+      s = 's'
+  else:
+      s =''
+  print(f'{i} bottle{s} of beer on the wall')
+  print(f'{i} bottle{s} of beer')
   print('Take one down, pass it around')
   if i-1 == 0:
     print('No more bottles of beer on the wall,')
