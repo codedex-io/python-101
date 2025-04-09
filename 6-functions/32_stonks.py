c845711d47132dd7e8353fc50ffb746db30e7e62
Shirish Krishna S <Shirishkrishna6969@gmail.com>
2025-04-09 22:33:00 +0530

    Update 32_stonks.py
    this is the correct code ur version would give index out of range error

--- 6-functions/32_stonks.py
+++ 6-functions/32_stonks.py
@@ -1,23 +1,9 @@
-# Stonks 📈
-# Codédex
+# Write code below 💖
 
 stock_prices = [34.68, 36.09, 34.94, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
-
-def price_at(i):
-  return stock_prices[i-1]
-
-def max_price(a, b):
-  mx = 0
-  for i in range(a, b + 1):
-    mx = max(mx, price_at(i))
-  return mx
-
-def min_price(a, b):
-  mn = price_at(a)
-  for i in range(a, b + 1):
-    mn = min(mn, price_at(i))
-  return mn
-
-print(max_price(1, 15))
-print(min_price(5, 10))
-print(price_at(3))
+def price_at(x):
+  return stock_prices[x-1]
+def max_price(a,b):
+  return max(stock_prices[a-1:b])
+def min_prices(a,b):
+  return min(stock_prices[a-1:b])
