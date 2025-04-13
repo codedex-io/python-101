8f9562cb9b26cac7783a8ac53f7e3b23871d41f1
Sonny Li <sonnynomnom@gmail.com>
2025-04-13 16:40:23 -0400

    Update 18_guess_number.py

--- 4-loops/18_guess_number.py
+++ 4-loops/18_guess_number.py
@@ -5,7 +5,7 @@ guess = 0
 tries = 0
 
 while guess != 6 and tries < 5:
-  guess = int(input('Guess the number:  '))
+  guess = int(input('Guess the number: '))
   tries = tries + 1
 
 if guess != 6:
