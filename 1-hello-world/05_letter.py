cff38852daf4084a47bbed9da38357fa1e2b033f
Sonny Li <sonnynomnom@gmail.com>
2025-10-29 16:56:38 -0400

    Update 05_letter.py

--- 1-hello-world/05_letter.py
+++ 1-hello-world/05_letter.py
@@ -8,7 +8,7 @@ print('|                                                                 Brookly
 print('|       Dear Self,                                                                |')
 print('|                                                                                 |')
 print('|       Does great art require suffering?                                         |')
-print('|       Does great product require capital?                                        |')
+print('|       Does great product require capital?                                       |')
 print('|       Did you find any truth at the end of this road?                           |')
 print('|                                                                                 |')
 print('|       Regardless, go build the learning platform you’ve always dreamed of —     |')
